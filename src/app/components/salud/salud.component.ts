084541dbbbaf86851378987f612a8e2ac8d1aead
Edward Llanca Haro <ellanca@belatrixsf.com>
2017-06-16 22:30:38 -0500

    add salud tabs

--- src/app/components/salud/salud.component.ts
+++ src/app/components/salud/salud.component.ts
@@ -6,10 +6,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./salud.component.css']
 })
 export class SaludComponent {
-	
+
 	 constructor(private route: ActivatedRoute) {}
 
-   ngOnInit() {
-    
-  }
+   ngOnInit() {}
+
+  public openCity(evt, cityName) {
+    let i, tabcontent, tablinks;
+		try{
+		    tabcontent = document.getElementsByClassName("tabcontent");
+		    for (i = 0; i < tabcontent.length; i++) {
+		        tabcontent[i].style.display = "none";
+		    }
+		    tablinks = document.getElementsByClassName("tablinks");
+		    for (i = 0; i < tablinks.length; i++) {
+		        tablinks[i].className = tablinks[i].className.replace(" active", "");
+		    }
+		    document.getElementById(cityName).style.display = "block";
+		    evt.currentTarget.className += " active";
+		}catch(er){
+			console.log(er)
+		}
+	}
 }
